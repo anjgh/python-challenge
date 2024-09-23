8d3e7cbaf581681d7d6a73cf65ae099a97790c9a
anjgh <anj.ghimi@gmail.com>
2024-09-23 15:40:47 -0400

    modified PyBank and PyPoll and output results to text file

--- PyBank/PyBank_starter.py
+++ PyBank/PyBank_starter.py
@@ -6,9 +6,6 @@ import csv
 #set path for file
 csvpath = os.path.join('Resources','budget_data.csv')
 
-print("Financial Analysis\n")
-print("----------------------------\n")
-
 total_months_count = 0
 net_total_profit_losses = 0
 average_change = []
@@ -18,26 +15,33 @@ greatest_increase_date = ''
 greatest_decrease = 0
 greatest_decrease_date = ''
 
+
 #open the CSV
 with open(csvpath, mode='r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    csv_header = next(csvfile)
-    first_profit_value = int(next(csvreader)[1])
-    
-    #Loop through looking for total number of months
-    for row in csvreader:
-        if row[0] != csv_header:
-            total_months_count = total_months_count + 1
-        if row[1] != csv_header:
-            #incrementally add the value of int(row[1]) to net_total_profit_losses
-            net_total_profit_losses = net_total_profit_losses + int(row[1]) 
+    #skip the header
+    next(csvfile)
 
+    #get the first row data
+    first_row = next(csvreader)
+    first_profit_value = int(first_row[1])
+    net_total_profit_losses = net_total_profit_losses + first_profit_value
+    total_months_count = total_months_count + 1
+    
+    for row in csvreader:
+        total_months_count = total_months_count + 1
         current_profit_value = int(row[1])   
+
+        #incrementally add the value of int(row[1]) to net_total_profit_losses
+        net_total_profit_losses = net_total_profit_losses + current_profit_value
+        
+        #calculate the change and add it to average_change list
         change =  current_profit_value - first_profit_value
         average_change.append(change)
         total_change = total_change + change
-          
+
+        #check for greatest increase and greatest decrease  
         if change > greatest_increase:
             greatest_increase = change
             greatest_increase_date = row[0]
@@ -45,13 +49,26 @@ with open(csvpath, mode='r') as csvfile:
             greatest_decrease = change
             greatest_decrease_date = row[0]    
 
+        #update first_profit_value to the current row's profit value
         first_profit_value = current_profit_value        
 
-
+    #calculate the average_change
     average_change = total_change/len(average_change)
 
-print(f"Total Months: {total_months_count}\n")
-print(f"Total: ${net_total_profit_losses}\n")
-print(f"Average Change: ${average_change}\n")
-print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})\n")
-print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})\n")
+#prepare output
+results = (
+    "\nFinancial Analysis\n"
+    "----------------------------\n"
+    f"Total Months: {total_months_count}\n"
+    f"Total: ${net_total_profit_losses}\n"
+    f"Average Change: ${average_change:.2f}\n"
+    f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})\n"
+    f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})\n"
+)
+
+#print the results
+print(results)
+
+output_path = os.path.join('analysis','financial_analysis.txt')
+with open(output_path, mode = 'w') as textfile:
+    textfile.write(results)
--- PyPoll/PyPoll_starter.py
+++ PyPoll/PyPoll_starter.py
@@ -3,8 +3,59 @@ import os
 #module for reading CSV files
 import csv
 
-csvpath = os.path.join('..','Resources','budget_data.csv')
+#set path for file
+csvpath = os.path.join('Resources','election_data.csv')
 
-with open(csvpath) as csvfile:
+total_votes_count = 0
+candidates = []
+candidate_vote_count = {}
+
+#open the CSV
+with open(csvpath, mode='r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+
+    csv_header = next(csvfile)
     
+    for row in csvreader:
+        total_votes_count = total_votes_count + 1 
+
+        candidate = row[2]    
+      
+        if candidate not in candidates:
+            #if the candidate is not already in the candidates list, then append candidate to candidates list
+            candidates.append(candidate)
+            #candidate is added as key and value initialized to 1
+            candidate_vote_count[candidate] = 1 
+
+        elif candidate in candidates:
+            # If the candidate is already in the dictionary, increments their vote count by 1
+            candidate_vote_count[candidate] = candidate_vote_count[candidate] + 1
+
+#max() function checks each candidate's vote count (value) and returns candidate (key) with the highest count
+winner = max(candidate_vote_count, key = candidate_vote_count.get)               
+
+results = (            
+    "\nElection Results\n"
+    "----------------------------\n"   
+    f"Total Votes: {total_votes_count}\n"
+    "----------------------------\n"
+)
+    #loop through both the candidate (key) and votes (value) in candidate_vote_count dictionary and print them
+    #calculate vote percentage by taking each votes (valte) and dividing it by total_votes_count
+    #print the candidate's name, vote percentage (formatted to 3 decimal places) and the total votes they received
+for candidate, votes in candidate_vote_count.items():
+    percentage = (votes / total_votes_count) * 100
+    results = results + f"{candidate}: {percentage:.3f}% ({votes})\n"
+
+results = results + (
+    "----------------------------\n"
+    f"Winner: {winner}\n"
+    "----------------------------\n"
+)
+
+#print the results
+print(results)
+
+output_path = os.path.join('analysis','election_analysis.txt')
+with open(output_path, mode = 'w') as textfile:
+    textfile.write(results)
